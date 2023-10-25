13ca31ba0788494cde442f42b555bcbea3649537
Axel Caceres <acaceres@c3r14s2.42madrid.com>
2023-10-26 01:10:41 +0200

    help_usage message improved

--- concepts/todo.md
+++ concepts/todo.md
@@ -1,4 +1,3 @@
-* set enum for fractals names
 * add the fractal bonus to help-usage function
 * set macros to some constant values
 * organize the header file
--- src/utils/print_help_usage.c
+++ src/utils/print_help_usage.c
@@ -6,7 +6,7 @@
 /*   By: acaceres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 05:06:16 by acaceres          #+#    #+#             */
-/*   Updated: 2023/10/21 05:59:52 by acaceres         ###   ########.fr       */
+/*   Updated: 2023/10/26 01:08:19 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 void	print_help_usage(void)
 {
-	ft_printf("\n");
-	ft_printf("Usage: ./fractol Mandelbrot | Julia <c-real> <c-imaginary>\n");
-	ft_printf("Render fractals using the specified type.\n");
-	ft_printf("\n");
-	ft_printf("Examples:\n");
-	ft_printf("./fractol Mandelbrot\n");
-	ft_printf("./fractol Julia 0.355 0.355\n");
-	ft_printf("\n");
+	printf("\n");
+	printf("Usage: ./fractol [fractal_type] [optional_arguments]\n");
+	printf("\n");
+	printf("Render fractal images with various types.\n");
+	printf("\n");
+	printf("Available Fractal Types:\n");
+	printf("\tMandelbrot:\t\tThe Mandelbrot Set\n");
+	printf("\tJulia [c-real c-imag]:\tThe Julia Set with custom constants\n");
+	printf("\tBurning Ship:\t\tThe Burning Ship Fractal\n");
+	printf("\n");
+	printf("Examples:\n");
+	printf("\t./fractol Mandelbrot\n");
+	printf("\t./fractol Julia -0.2 0.89\n");
+	printf("\t./fractol \"Burning_Ship\"\n");
+	printf("\n");
 	exit(EXIT_FAILURE);
 }
